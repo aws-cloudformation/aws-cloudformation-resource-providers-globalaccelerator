ea3d7cf36919353a52048a99eb32dc20862f4d30
DhirajChaudhary <dhirach@amazon.com>
2020-03-06 09:08:43 -0800

    Schema and handler for CREATE accelerator

---
+++ aws-aga-accelerator/src/main/java/software/amazon/globalaccelerator/accelerator/AcceleratorClientBuilder.java
@@ -0,0 +1,10 @@
+package software.amazon.globalaccelerator.accelerator;
+import com.amazonaws.services.globalaccelerator.AWSGlobalAccelerator;
+import com.amazonaws.services.globalaccelerator.AWSGlobalAcceleratorClientBuilder;
+
+public class AcceleratorClientBuilder {
+    private static final String SUPPORTED_REGION = "us-west-2";
+    static AWSGlobalAccelerator getClient() {
+        return AWSGlobalAcceleratorClientBuilder.standard().withRegion(SUPPORTED_REGION).build();
+    }
+}
