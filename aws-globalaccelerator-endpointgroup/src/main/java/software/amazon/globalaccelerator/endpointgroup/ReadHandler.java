e8100d0a4e453a69b5347f18705353a4d1c0dca7
DhirajChaudhary <dhirach@amazon.com>
2020-03-31 18:10:47 -0700

    Added read handler and unit tests for endpoint groups

--- aws-globalaccelerator-endpointgroup/src/main/java/software/amazon/globalaccelerator/endpointgroup/ReadHandler.java
+++ aws-globalaccelerator-endpointgroup/src/main/java/software/amazon/globalaccelerator/endpointgroup/ReadHandler.java
@@ -1,6 +1,9 @@
 package software.amazon.globalaccelerator.endpointgroup;
 
+import com.amazonaws.services.globalaccelerator.model.EndpointGroup;
+import lombok.val;
 import software.amazon.cloudformation.proxy.AmazonWebServicesClientProxy;
+import software.amazon.cloudformation.proxy.HandlerErrorCode;
 import software.amazon.cloudformation.proxy.Logger;
 import software.amazon.cloudformation.proxy.ProgressEvent;
 import software.amazon.cloudformation.proxy.OperationStatus;
@@ -17,11 +20,33 @@ public class ReadHandler extends BaseHandler<CallbackContext> {
 
         final ResourceModel model = request.getDesiredResourceState();
 
-        // TODO : put your code here
+        val agaClient = AcceleratorClientBuilder.getClient();
+        logger.log(String.format("Read request for endpoint group: [%s]", request));
 
-        return ProgressEvent.<ResourceModel, CallbackContext>builder()
-            .resourceModel(model)
-            .status(OperationStatus.SUCCESS)
-            .build();
+        val endpointGroup = HandlerCommons.getEndpointGroup(model.getEndpointGroupArn(), proxy, agaClient, logger);
+        val endpointGroupResourceModel = convertEndpointGroupToResourceModel(endpointGroup);
+
+        if (endpointGroupResourceModel == null){
+            logger.log(String.format("Endpoint group with ARN [%s] not found", model.getEndpointGroupArn()));
+            return ProgressEvent.defaultFailureHandler(new Exception("Endpoint group not found."), HandlerErrorCode.NotFound);
+        } else {
+            return ProgressEvent.defaultSuccessHandler(endpointGroupResourceModel);
+        }
+    }
+
+    private ResourceModel convertEndpointGroupToResourceModel(EndpointGroup endpointGroup) {
+        ResourceModel newModel = null;
+        if (endpointGroup != null) {
+            newModel = new ResourceModel();
+            newModel.setEndpointGroupArn(endpointGroup.getEndpointGroupArn());
+            newModel.setHealthCheckIntervalSeconds(endpointGroup.getHealthCheckIntervalSeconds());
+            newModel.setHealthCheckPath(endpointGroup.getHealthCheckPath());
+            newModel.setHealthCheckPort(endpointGroup.getHealthCheckPort());
+            newModel.setHealthCheckProtocol(endpointGroup.getHealthCheckProtocol());
+            newModel.setThresholdCount(endpointGroup.getThresholdCount());
+            newModel.setTrafficDialPercentage(endpointGroup.getTrafficDialPercentage().intValue());
+            newModel.setEndpointGroupRegion(endpointGroup.getEndpointGroupRegion());
+        }
+        return newModel;
     }
 }
--- aws-globalaccelerator-endpointgroup/src/test/java/software/amazon/globalaccelerator/endpointgroup/ReadHandlerTest.java
+++ aws-globalaccelerator-endpointgroup/src/test/java/software/amazon/globalaccelerator/endpointgroup/ReadHandlerTest.java
@@ -1,14 +1,29 @@
 package software.amazon.globalaccelerator.endpointgroup;
 
+import com.amazonaws.services.globalaccelerator.model.DescribeEndpointGroupRequest;
+import com.amazonaws.services.globalaccelerator.model.DescribeEndpointGroupResult;
+import com.amazonaws.services.globalaccelerator.model.EndpointGroup;
+import com.amazonaws.services.globalaccelerator.model.EndpointGroupNotFoundException;
+import lombok.val;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import software.amazon.cloudformation.proxy.AmazonWebServicesClientProxy;
+import software.amazon.cloudformation.proxy.HandlerErrorCode;
 import software.amazon.cloudformation.proxy.Logger;
+import software.amazon.cloudformation.proxy.OperationStatus;
+import software.amazon.cloudformation.proxy.ResourceHandlerRequest;
 
+import java.util.ArrayList;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.doThrow;
 
 @ExtendWith(MockitoExtension.class)
 public class ReadHandlerTest {
@@ -25,7 +40,59 @@ public class ReadHandlerTest {
         logger = mock(Logger.class);
     }
 
+    private ResourceModel createTestResourceModel() {
+        val endpointConfigurations = new ArrayList<EndpointConfiguration>();
+        endpointConfigurations.add(EndpointConfiguration.builder().endpointId("EPID1").build());
+        endpointConfigurations.add(EndpointConfiguration.builder().endpointId("EPID2").build());
+
+        return ResourceModel.builder()
+                .endpointGroupArn("ENDPOINTGROUP_ARN")
+                .endpointGroupRegion("us-west-2")
+                .listenerArn("LISTENER_ARN")
+                .healthCheckPort(10)
+                .thresholdCount(100)
+                .endpointConfigurations(endpointConfigurations)
+                .healthCheckPath("/Health")
+                .trafficDialPercentage(100)
+                .healthCheckIntervalSeconds(10)
+                .healthCheckProtocol("TCP")
+                .listenerArn("arn:aws:globalaccelerator::474880776455:accelerator/abcd1234/listener/12341234")
+                .build();
+    }
+
     @Test
-    public void handleRequest_SimpleSuccess() {
+    public void handleRequest_returnsMappedEndpointGroup() {
+        ResourceModel model = createTestResourceModel();
+        DescribeEndpointGroupResult describeEndpointGroupResult = new DescribeEndpointGroupResult()
+                .withEndpointGroup(new EndpointGroup()
+                        .withEndpointGroupArn("ENDPOINTGROUP_ARN")
+                        .withEndpointGroupRegion("us-west-2")
+                        .withTrafficDialPercentage(50.0f));
+
+        doReturn(describeEndpointGroupResult).when(proxy).injectCredentialsAndInvoke(any(DescribeEndpointGroupRequest.class), any());
+
+        val request = ResourceHandlerRequest.<ResourceModel>builder().desiredResourceState(model).build();
+        val response = new ReadHandler().handleRequest(proxy, request, null, logger);
+
+        Assertions.assertNotNull(response);
+        Assertions.assertEquals(response.getStatus(), OperationStatus.SUCCESS);
+        Assertions.assertNotNull(response.getResourceModel());
+        Assertions.assertNull(response.getCallbackContext());
+        Assertions.assertEquals(response.getResourceModel().getEndpointGroupArn(), "ENDPOINTGROUP_ARN");
+        Assertions.assertEquals(response.getResourceModel().getEndpointGroupRegion(), "us-west-2");
+    }
+
+    @Test
+    public void handleRequest_nonExistingEndpointGroup() {
+        ResourceModel model = createTestResourceModel();
+        doThrow(new EndpointGroupNotFoundException("NOT FOUND")).when(proxy).
+                injectCredentialsAndInvoke(any(DescribeEndpointGroupRequest.class), any());
+        val request = ResourceHandlerRequest.<ResourceModel>builder().desiredResourceState(model).build();
+        val response = new ReadHandler().handleRequest(proxy, request, null, logger);
+
+        Assertions.assertNotNull(response);
+        Assertions.assertEquals(response.getStatus(), OperationStatus.FAILED);
+        Assertions.assertEquals(response.getErrorCode(), HandlerErrorCode.NotFound);
+        Assertions.assertNull(response.getResourceModel());
     }
 }
