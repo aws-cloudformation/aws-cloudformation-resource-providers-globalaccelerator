993560b6df234d20a03440275ece2667b50daebd
Akshat Aranya <aranyaa@amazon.com>
2020-04-03 08:28:09 -0700

    Use MockK for accelerator

--- aws-globalaccelerator-accelerator/src/test/kotlin/software/amazon/globalaccelerator/accelerator/ReadHandlerTest.kt
+++ aws-globalaccelerator-accelerator/src/test/kotlin/software/amazon/globalaccelerator/accelerator/ReadHandlerTest.kt
@@ -1,15 +1,14 @@
 package software.amazon.globalaccelerator.accelerator
 
-import com.amazonaws.AmazonWebServiceResult
-import com.amazonaws.ResponseMetadata
 import com.amazonaws.services.globalaccelerator.model.Accelerator
 import com.amazonaws.services.globalaccelerator.model.AcceleratorNotFoundException
 import com.amazonaws.services.globalaccelerator.model.AcceleratorStatus
-import com.amazonaws.services.globalaccelerator.model.CreateAcceleratorRequest
-import com.amazonaws.services.globalaccelerator.model.DescribeAcceleratorRequest
 import com.amazonaws.services.globalaccelerator.model.DescribeAcceleratorResult
 import com.amazonaws.services.globalaccelerator.model.IpSet
-import org.assertj.core.api.Assertions
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.junit5.MockKExtension
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Assertions.assertNull
@@ -17,33 +16,27 @@ import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
-import org.mockito.ArgumentMatchers
-import org.mockito.Mock
-import org.mockito.Mockito
-import org.mockito.Mockito.doThrow
-import org.mockito.junit.jupiter.MockitoExtension
 import software.amazon.cloudformation.proxy.AmazonWebServicesClientProxy
 import software.amazon.cloudformation.proxy.HandlerErrorCode
 import software.amazon.cloudformation.proxy.Logger
 import software.amazon.cloudformation.proxy.OperationStatus
-import software.amazon.cloudformation.proxy.ProgressEvent
 import software.amazon.cloudformation.proxy.ResourceHandlerRequest
-import java.util.*
-import java.util.function.Function
 
-@ExtendWith(MockitoExtension::class)
+@ExtendWith(MockKExtension::class)
 class ReadHandlerTest {
-    @Mock
-    private var proxy: AmazonWebServicesClientProxy? = null
+    @MockK
+    lateinit var proxy: AmazonWebServicesClientProxy
 
-    @Mock
-    private var logger: Logger? = null
+    @MockK(relaxed = true)
+    lateinit var logger: Logger
+
+    @BeforeEach
+    fun setup() = MockKAnnotations.init(this)
+
     private val ACCELERATOR_ARN = "arn:aws:globalaccelerator::444607872184:accelerator/88127aa5-01d8-484c-80a0-349daaefce1d"
     private var ipSet: IpSet? = null
     private fun createTestResourceModel(): ResourceModel {
-        val ipAddresses = mutableListOf<String>()
-        ipAddresses.add("10.10.10.1")
-        ipAddresses.add("10.10.10.2")
+        val ipAddresses = listOf("10.10.10.1", "10.10.10.2")
         ipSet = IpSet().withIpAddresses(ipAddresses)
         return ResourceModel.builder()
                 .acceleratorArn(ACCELERATOR_ARN)
@@ -52,12 +45,6 @@ class ReadHandlerTest {
                 .ipAddresses(ipAddresses)
                 .ipAddressType("IPV4")
                 .build()
-    }
-
-    @BeforeEach
-    fun setup() {
-        proxy = Mockito.mock(AmazonWebServicesClientProxy::class.java)
-        logger = Mockito.mock(Logger::class.java)
     }
 
     @Test
@@ -72,10 +59,10 @@ class ReadHandlerTest {
                         .withName("Name")
                         .withIpSets(ipSet)
                 )
-        Mockito.doReturn(describeAcceleratorResult).`when`(proxy)!!.injectCredentialsAndInvoke(ArgumentMatchers.any(DescribeAcceleratorRequest::class.java),
-                ArgumentMatchers.any<Function<DescribeAcceleratorRequest, AmazonWebServiceResult<ResponseMetadata>>>())
+        every { proxy.injectCredentialsAndInvoke(ofType(), ofType<ProxyDescribeAccelerator>()) } returns describeAcceleratorResult
+
         val request = ResourceHandlerRequest.builder<ResourceModel>().desiredResourceState(model).build()
-        val response = ReadHandler().handleRequest(proxy!!, request, null, logger!!)
+        val response = ReadHandler().handleRequest(proxy, request, null, logger)
         assertNotNull(response)
         assertEquals(OperationStatus.SUCCESS, response.status)
         assertNull(response.callbackContext)
@@ -92,10 +79,10 @@ class ReadHandlerTest {
     @Test
     fun handleRequest_nonExistingAccelerator() {
         val model = createTestResourceModel()
-        doThrow(AcceleratorNotFoundException("NOT FOUND")).`when`(proxy)!!.injectCredentialsAndInvoke(ArgumentMatchers.any(DescribeAcceleratorRequest::class.java),
-                ArgumentMatchers.any<Function<DescribeAcceleratorRequest, AmazonWebServiceResult<ResponseMetadata>>>())
+        every { proxy.injectCredentialsAndInvoke(ofType(), ofType<ProxyDescribeAccelerator>()) } throws AcceleratorNotFoundException("NOT FOUND")
+
         val request = ResourceHandlerRequest.builder<ResourceModel>().desiredResourceState(model).build()
-        val response = ReadHandler().handleRequest(proxy!!, request, null, logger!!)
+        val response = ReadHandler().handleRequest(proxy, request, null, logger)
         assertNotNull(response)
         assertEquals(OperationStatus.FAILED, response.status)
         assertEquals(HandlerErrorCode.NotFound, response.errorCode)
