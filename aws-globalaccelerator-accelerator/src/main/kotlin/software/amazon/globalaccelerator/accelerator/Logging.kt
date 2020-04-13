b0f719389cf6510ca278711cf54ea7a6b879fb3e
DhirajChaudhary <dhirach@amazon.com>
2020-04-13 13:22:49 -0700

    Converting concatenation to template

--- aws-globalaccelerator-accelerator/src/main/kotlin/software/amazon/globalaccelerator/accelerator/Logging.kt
+++ aws-globalaccelerator-accelerator/src/main/kotlin/software/amazon/globalaccelerator/accelerator/Logging.kt
@@ -3,11 +3,11 @@ package software.amazon.globalaccelerator.accelerator
 import software.amazon.cloudformation.proxy.Logger
 
 fun Logger.logInfo(str: String) {
-    this.log("[INFO]" + str)
+    this.log("[INFO] - $str")
 }
 fun Logger.logError(str: String) {
-    this.log("[ERROR]" + str)
+    this.log("[ERROR] - $str")
 }
 fun Logger.logDebug(str: String) {
-    this.log("[DEBUG]" + str)
+    this.log("[DEBUG] - $str")
 }
